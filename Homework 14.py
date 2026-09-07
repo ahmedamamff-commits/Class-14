e4b31b5eb4b3cca1c87fbe3631402e884e2e29be
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-09-07 08:51:39 -0700

    Add files via upload

---
+++ Homework 14.py
@@ -0,0 +1,4 @@
+def shutdown_function():
+    print("The shutdown function is a function that is called when the program is about to terminate, or end. It is used to perform any necessary cleanup tasks, such as closing files or saving data before exiting the program.")
+
+shutdown_function()
