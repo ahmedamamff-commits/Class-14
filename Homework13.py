67b544308455f3d0f5a1018b997cc973ac41aae9
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-09-07 08:51:10 -0700

    Add files via upload

---
+++ Homework13.py
@@ -0,0 +1,8 @@
+def Circle_Circumference(radius):
+    """
+    Calculates the total circumference of a circle that has a given radius."""
+
+    circumference = 2 * 3.14159 * radius
+    return Circle_Circumference
+
+print(Circle_Circumference(5))
